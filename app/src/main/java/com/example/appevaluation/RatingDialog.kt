a68c06c59bfc1e37bb2397dabc85eec27ce8a008
Eugene <e.ponomarev@mstrike.ru>
2020-09-21 16:38:58 +0300

    Оценка приложения

---
+++ app/src/main/java/com/example/appevaluation/RatingDialog.kt
@@ -0,0 +1,15 @@
+package com.example.appevaluation
+
+import android.content.Context
+import android.os.Parcelable
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+
+class RatingDialog(context: Context) : AlertDialog(context) {
+
+
+    init {
+        val inflater = LayoutInflater.from(context)
+        //inflater.inflate(R.layout.reting_dialog, this)
+    }
+}
