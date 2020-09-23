00dda2e6409400ef89506ba4b38d266d5d40db92
EugenePonomarev <e.ponomarev@mstrike.ru>
2020-09-23 11:09:22 +0300

    Сейчас рейтинг работает, не нужный код убрал

--- app/src/main/java/com/example/appevaluation/MainActivity.kt
+++ app/src/main/java/com/example/appevaluation/MainActivity.kt
@@ -3,36 +3,20 @@ package com.example.appevaluation
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 
-
-lateinit var button1: Button
-lateinit var button2: Button
-lateinit var rl: RelativeLayout
-
+lateinit var button: Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button1 = findViewById(R.id.main_activity_button_ration_dialog)
-        button2 = findViewById(R.id.main_activity_button_ration_dialog_fragment)
-        rl = findViewById(R.id.main_activity_fragment)
+        button = findViewById(R.id.main_activity_button_ration_dialog_fragment)
 
-        button1.setOnClickListener {
-            RatingDialog(this).show()
-        }
-        button2.setOnClickListener {
-            val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
-            val fragment: Fragment = RatingDialogDialogFragment().newInstance()!!
-            ft.add(R.id.main_activity_fragment, fragment)
-            ft.commit()
-
+        button.setOnClickListener {
+            val dialog = RatingDialogDialogFragment()
+            val manager = supportFragmentManager
+            dialog.show(manager, "dialog")
         }
     }
 }
--- app/src/main/java/com/example/appevaluation/RatingDialogDialogFragment.kt
+++ app/src/main/java/com/example/appevaluation/RatingDialogDialogFragment.kt
@@ -1,14 +1,21 @@
 package com.example.appevaluation
 
+import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
+import com.example.appevaluation.RatingDialogDialogFragment.States
 
+
+/**
+ * Диалоговое окно с рейтингом приложения
+ * Если пользователь поставил от 1 до 3 то мы выводим окно с отзывом [States.REVIEW_BODY]
+ * Усли пользователь стаит 4 или 5 отправляем его оценку [States.THANKS_BODY]
+ */
 class RatingDialogDialogFragment : DialogFragment() {
     /**
      * Слой для тела оценки
@@ -33,18 +40,49 @@ class RatingDialogDialogFragment : DialogFragment() {
     private lateinit var llThanksBody: LinearLayout //rating_dialog_thanks_layout_linear_layout
     private lateinit var tvCloseThanksWindows: TextView //rating_dialog_body_thanks_close_text_view
 
-    fun newInstance(): RatingDialogDialogFragment? {
-        return RatingDialogDialogFragment()
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.reting_dialog, container, false)
-    }
+    enum class States { RATE_BODY, REVIEW_BODY, THANKS_BODY }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    /**
+     * Состояние Окна
+     */
+    private var status = States.RATE_BODY
+        set(value) {
+            field = value
+            when (status) {
+                States.RATE_BODY -> {
+                    llRateBody.visibility = View.VISIBLE
+                    llReviewBody.visibility = View.GONE
+                    llThanksBody.visibility = View.GONE
+                }
+                States.REVIEW_BODY -> {
+                    llRateBody.visibility = View.GONE
+                    llReviewBody.visibility = View.VISIBLE
+                    llThanksBody.visibility = View.GONE
+                }
+                States.THANKS_BODY -> {
+                    llRateBody.visibility = View.GONE
+                    llReviewBody.visibility = View.GONE
+                    llThanksBody.visibility = View.VISIBLE
+                }
+            }
+        }
+
+    /**
+     * Сообщение написанное пользователем для разработчиков
+     */
+    private lateinit var message: String
+
+    /**
+     * оценка пользователя
+     */
+    private var score: Float = 0F
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = context?.let { AlertDialog.Builder(it, R.style.AlertDialog) }
+        val inflater = activity!!.layoutInflater
+        val view: View = inflater.inflate(R.layout.reting_dialog, null)
+        builder?.setView(view)
 
         llRateBody = view.findViewById(R.id.rating_dialog_body_rate_linear_layout)
         ratingBar = view.findViewById(R.id.rating_dialog_body_rate_ratingBar)
@@ -63,11 +101,51 @@ class RatingDialogDialogFragment : DialogFragment() {
         llThanksBody.visibility = View.GONE
         tvCloseThanksWindows = view.findViewById(R.id.rating_dialog_body_thanks_close_text_view)
 
-
-
         bEvaluation.setOnClickListener {
-            Toast.makeText(context, "score", Toast.LENGTH_SHORT).show()
-        }
-    }
+            when (ratingBar.rating) {
+                0F -> tvRateScore.visibility = View.VISIBLE
+                in 1F..3F -> {
+                    score = ratingBar.rating
+                    status = States.REVIEW_BODY
+                }
+                else -> {
+                    // метод для отправки рейтинга
 
+                    startActivity(Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=ru.tns.tnsmobile")))
+                }
+            }
+        }
+
+        bSendEvaluation.setOnClickListener {
+            if (etReviewTyping.text.isNotEmpty()) {
+                message = etReviewTyping.toString()
+
+                // метод для отправки рейтинга
+
+                status = States.THANKS_BODY
+            } else {
+                etReviewTyping.let {
+                    it.hint =
+                        activity?.getString(R.string.rating_dialog_text_hint_body_review_edit_text)
+                        activity?.resources?.getColor(R.color.colorTextRed)
+                        ?.let { it1 -> it.setHintTextColor(it1) }
+                }
+            }
+        }
+
+
+        tvEvaluationLate.setOnClickListener {
+            //Метод для формирования другой даты отзыва
+        }
+
+        tvCloseReviewWindow.setOnClickListener { dialog?.cancel() }
+        tvCloseThanksWindows.setOnClickListener { dialog?.cancel() }
+
+
+        if (builder != null) {
+            return builder.create()
+        }
+        return super.onCreateDialog(savedInstanceState)
+    }
 }
